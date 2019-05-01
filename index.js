97ba1dcfa07c28694511d22d7513823dd84eb917
Bruno Buccolo <bruno.buccolo@gmail.com>
2019-05-01 11:35:10 -0300

    Sync test

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 const express = require('express')
 const helmet = require('helmet')
-const axios = require('axios')
+const superagent = require('superagent')
 const cheerio = require('cheerio')
-
-var http = require('http');
-var agent = new http.Agent({ family: 4 });
-axios.defaults.httpAgent = agent;
 
 const app = express()
 
 var data = "loading...";
-axios.get('https://www.globo.com')
-.then(response => {
-  data = cheerio.load(response.data)('h1').text();
-})
-.catch(error => {
-  data = error;
-  console.log('err:' + error);
-});
+console.log(data);
 
 // add some security-related headers to the response
 app.use(helmet())
 
 app.get('*', (req, res) => {
     res.set('Content-Type', 'text/html')
-    res.status(200).send(`${data}`)
+    res.set('Cache-Control', 'no-cache')
+
+    superagent
+    .get('https://www.globo.com')
+    .then(response => {
+      data = cheerio.load(response.text)('h1').text();
+      res.status(200).send(`${data}`)
+    })
+    .catch(error => {
+      console.log('wtf2');
+      data = error;
+      res.status(200).send(`${data}`)
+      console.log('err:' + error);
+    });
 })
 
 module.exports = app
