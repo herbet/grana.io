258a91daf2715aa3eb63699488270862595933dd
Bruno Buccolo <bruno.buccolo@gmail.com>
2019-05-01 12:34:17 -0300

    Add build

---
+++ blog/_site/build.sh
@@ -0,0 +1,23 @@
+if [ "$NODE_ENV" == "production" ]; then
+  yum install -y readline-devel openssl-devel zlib-devel
+
+  git clone git://github.com/rbenv/rbenv.git ~/.rbenv
+  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+  echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+  source ~/.bashrc
+
+  git clone git://github.com/rbenv/ruby-build.git /tmp/ruby-build
+  pushd /tmp/ruby-build
+  ./install.sh
+  popd
+
+  rbenv install 2.5.5 && rbenv global 2.5.5
+
+  gem install jekyll bundler:1.16.3
+
+  bundle install
+
+  bundle exec jekyll build
+else
+  bundle exec jekyll build
+fi
