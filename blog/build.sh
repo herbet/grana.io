52fcf6983593a5f8936bd0b255a722cecb95ef75
Bruno Buccolo <bruno.buccolo@gmail.com>
2019-05-01 12:16:01 -0300

    Monorepo testing

--- blog/build.sh
+++ blog/build.sh
@@ -1,19 +1,23 @@
-yum install -y readline-devel openssl-devel zlib-devel
+if [ "$NODE_ENV" == "production" ]; then
+  yum install -y readline-devel openssl-devel zlib-devel
 
-git clone git://github.com/rbenv/rbenv.git ~/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-source ~/.bashrc
+  git clone git://github.com/rbenv/rbenv.git ~/.rbenv
+  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+  echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+  source ~/.bashrc
 
-git clone git://github.com/rbenv/ruby-build.git /tmp/ruby-build
-pushd /tmp/ruby-build
-./install.sh
-popd
+  git clone git://github.com/rbenv/ruby-build.git /tmp/ruby-build
+  pushd /tmp/ruby-build
+  ./install.sh
+  popd
 
-rbenv install 2.5.5 && rbenv global 2.5.5
+  rbenv install 2.5.5 && rbenv global 2.5.5
 
-gem install jekyll bundler:1.16.3
+  gem install jekyll bundler:1.16.3
 
-bundle install
+  bundle install
 
-bundle exec jekyll build
+  bundle exec jekyll build
+else
+  bundle exec jekyll serve
+fi
