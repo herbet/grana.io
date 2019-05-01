727a4bbb70cb2408dbf1abb6129a1f84b348aa1f
Bruno Buccolo <bruno.buccolo@gmail.com>
2019-04-30 23:32:30 -0300

    experiment

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express')
 const helmet = require('helmet')
+const axios = require('axios')
+const cheerio = require('cheerio')
 
 const app = express()
+
+var data = "loading...";
+axios.get('https://www.globo.com')
+.then(response => {
+  data = cheerio.load(response.data)('h1').text();
+})
+.catch(error => {
+  data = error;
+  console.log('err:' + error);
+});
 
 // add some security-related headers to the response
 app.use(helmet())
 
 app.get('*', (req, res) => {
     res.set('Content-Type', 'text/html')
-    res.status(200).send(`
-        <h1><marquee direction=right>Hello from Express path '/' on Now 2.0!</marquee></h1>
-        <h2>Go to <a href="/about">/about</a></h2>
-    `)
+    res.status(200).send(`${data}`)
 })
 
 module.exports = app
