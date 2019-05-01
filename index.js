d28054f1c52b236acc43f6ebb2f001039c1c29ec
Bruno Buccolo <bruno.buccolo@gmail.com>
2019-05-01 11:39:50 -0300

    Async no-cache test

--- index.js
+++ index.js
@@ -8,25 +8,22 @@ const app = express()
 var data = "loading...";
 console.log(data);
 
+superagent
+.get('https://www.globo.com')
+.then(response => {
+  data = cheerio.load(response.text)('h1').text();
+})
+.catch(error => {
+  data = error;
+});
+
 // add some security-related headers to the response
 app.use(helmet())
 
 app.get('*', (req, res) => {
     res.set('Content-Type', 'text/html')
-    res.set('Cache-Control', 'no-cache')
-
-    superagent
-    .get('https://www.globo.com')
-    .then(response => {
-      data = cheerio.load(response.text)('h1').text();
-      res.status(200).send(`${data}`)
-    })
-    .catch(error => {
-      console.log('wtf2');
-      data = error;
-      res.status(200).send(`${data}`)
-      console.log('err:' + error);
-    });
+    res.set('Cache-Control', 's-maxage=0, max-age=0')
+    res.status(200).send(`${data}`)
 })
 
 module.exports = app
